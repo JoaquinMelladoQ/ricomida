4befd64bc3d6fab5848ecb1ebd13fb4259bce513
joaco <mellado.joaquin@gmail.com>
2020-08-07 23:27:06 -0400

    first commit ricomida

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip({
+        html: true
+    })
+  })
+
+$('.carousel').carousel({
+    interval: 4000,
+    pause: false,
+    wrap: true
+})
